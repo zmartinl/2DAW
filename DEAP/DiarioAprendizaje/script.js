147c758bdc3476dad2be35053d13f8a9315f933a
zmartinl <147615414+zmartinl@users.noreply.github.com>
2024-09-26 22:49:57 +0200

    Add files via upload

---
+++ DEAP/DiarioAprendizaje/script.js
@@ -0,0 +1,25 @@
+gsap.to("#bg",{
+    scrollTrigger:{
+        scrub:1
+    },
+    scale:1.5,
+});
+gsap.to("#text",{
+    scrollTrigger:{
+        scrub: 1
+    },
+    y:250,
+});
+gsap.to("#nombre",{
+    scrollTrigger:{
+        scrub:1
+    },
+    x:-500,
+});
+gsap.to("#git",{
+    scrollTrigger:{
+        scrub:1
+    },
+    rotation: -100,
+    y: 500,
+});
