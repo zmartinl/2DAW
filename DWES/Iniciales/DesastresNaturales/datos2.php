6ab53aa47e32ac0aa93b41dfed68b9fd487f64c7
zmartinl <147615414+zmartinl@users.noreply.github.com>
2024-10-01 10:10:58 +0200

    Add files via upload

---
+++ DWES/Iniciales/DesastresNaturales/datos2.php
@@ -0,0 +1,9 @@
+<?php
+    print_r($_POST);
+    echo '<br/> <br/>';
+    var_dump($_POST);
+    echo '<br/> <br/>';
+    print_r($_POST['info']);
+    echo '<br/> <br/>';
+    var_dump($_POST['info']);
+?>
