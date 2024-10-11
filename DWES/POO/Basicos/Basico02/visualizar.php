24315fec606ee7b97b2fe41ae4a0c2183ffda9a9
zmartinl <zmartinllera@gmail.com>
2024-10-11 02:29:45 +0200

    Cambio en el visualizar

--- DWES/POO/Basicos/Basico02/visualizar.php
+++ DWES/POO/Basicos/Basico02/visualizar.php
@@ -8,23 +8,9 @@
 
     $objDiasAño = new DiasAño($año);
     // print_r($objDiasAño->mesesDias);
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Visualizacion</title>
-    </head>
-    <body>
-        <p>
-            <?php
-                echo 'La fecha introducida es: '.$dia.' de '.$objDiasAño->getMesesDias($mes)['mes'].' de '.$año.'
+    echo 'La fecha introducida es: '.$dia.' de '.$objDiasAño->getMesesDias($mes)['mes'].' de '.$año.'
                 <br> Febrero tiene: '.$objDiasAño->getMesesDias(2)['dias'].' dias';
-            ?>
-        </p>
-    </body>
-</html>
+
 
 
