e4fb4dcd98189657336b2415062fef4cdbcd39cc
zmartinl <zmartinllera@gmail.com>
2024-10-14 12:22:36 +0200

    Arreglado el problema al parsear numero del mes

--- DWES/POO/Basicos/Basico02/visualizar.php
+++ DWES/POO/Basicos/Basico02/visualizar.php
@@ -3,7 +3,7 @@
     require_once 'DiasAño.php';
     $fecha = explode('/',$_POST['fecha']);
     $dia = $fecha[0];
-    $mes = $fecha[1];
+    $mes = intval($fecha[1]); // Parseamos a int el mes ya que si introducimos /07/ cuando uso el mes en los metodos da error.
     $año = $fecha[2];
 
     $objDiasAño = new DiasAño($año);
